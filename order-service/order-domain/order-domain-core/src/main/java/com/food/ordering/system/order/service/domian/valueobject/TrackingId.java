fac5fafa77bf718080db4327852f7f74fde1e543
dmitry-lee <AlisaLev@1>
2023-04-01 19:05:40 +0300

    Order, OrderItem initialization and validation

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domian/valueobject/TrackingId.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domian/valueobject/TrackingId.java
@@ -1,13 +1,12 @@
 package com.food.ordering.system.order.service.domian.valueobject;
 
-import com.food.ordering.system.domain.entity.BaseEntity;
 import com.food.ordering.system.domain.valueobject.BaseId;
 
 import java.util.UUID;
 
 public class TrackingId extends BaseId<UUID> {
 
-    protected TrackingId(UUID value) {
+    public TrackingId(UUID value) {
         super(value);
     }
 }
