bb62197e4b2375d458150bb7a9a3c747870ede0e
dmitry-lee <dmitry.lee@yandex.ru>
2023-04-27 23:26:42 +0700

    payment-application-service: add mapper, config, ports

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/dto/PaymentRequest.java
@@ -0,0 +1,25 @@
+package com.food.ordering.system.payment.service.domain.dto;
+
+import com.food.ordering.system.domain.valueobject.PaymentOrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class PaymentRequest {
+
+    private String id;
+    private String sagaId;
+    private String orderId;
+    private String customerId;
+    private BigDecimal price;
+    private PaymentOrderStatus paymentOrderStatus;
+
+    public void setPaymentOrderStatus(PaymentOrderStatus paymentOrderStatus) {
+        this.paymentOrderStatus = paymentOrderStatus;
+    }
+}
