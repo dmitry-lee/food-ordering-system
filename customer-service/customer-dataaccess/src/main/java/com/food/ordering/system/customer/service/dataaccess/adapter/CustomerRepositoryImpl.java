611c130ffa34853561a6ddbf1542b598cbeaad9e
dmitry-lee <dmitry.lee@yandex.ru>
2023-07-03 00:32:57 +0700

    implement customer service modules

---
+++ customer-service/customer-dataaccess/src/main/java/com/food/ordering/system/customer/service/dataaccess/adapter/CustomerRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.customer.service.dataaccess.adapter;
+
+import com.food.ordering.system.customer.service.dataaccess.mapper.CustomerDataaccessMapper;
+import com.food.ordering.system.customer.service.dataaccess.repository.CustomerJpaRepository;
+import com.food.ordering.system.customer.service.domain.entity.Customer;
+import com.food.ordering.system.customer.service.domain.ports.output.repository.CustomerRepository;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+    private final CustomerJpaRepository customerJpaRepository;
+    private final CustomerDataaccessMapper customerDataaccessMapper;
+
+    public CustomerRepositoryImpl(CustomerJpaRepository customerJpaRepository,
+                                  CustomerDataaccessMapper customerDataaccessMapper) {
+        this.customerJpaRepository = customerJpaRepository;
+        this.customerDataaccessMapper = customerDataaccessMapper;
+    }
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return customerDataaccessMapper.customerEntityToCustomer(
+                customerJpaRepository.save(customerDataaccessMapper.customerToCustomerEntity(customer)));
+    }
+}
---
+++ customer-service/customer-domain/customer-application-service/src/main/java/com/food/ordering/system/customer/service/domain/create/CreateCustomerCommand.java
@@ -0,0 +1,22 @@
+package com.food.ordering.system.customer.service.domain.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateCustomerCommand {
+    @NotNull
+    private UUID customerId;
+    @NotNull
+    private String username;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+}
